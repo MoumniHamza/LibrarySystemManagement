99e9d36101ad6a10348e30ee4e25b5009295b879
Hamza Moumni <94747@supinfo.com>
2015-09-11 10:22:10 -0700

    drill n 2

---
+++ Drill2.sql
@@ -0,0 +1,15 @@
+
+/* How many copies of the book titled The Lost Tribe are owned by each library branch? */USE LibraryManagementSystemGO
+
+
+SELECT dbo.book.title,
+       dbo.library_branch.branch_name,
+	   dbo.book_copies.no_of_copies
+
+FROM dbo.book INNER JOIN dbo.book_copies
+ON dbo.book.bookid = dbo.book_copies.bookid
+INNER JOIN dbo.library_branch
+ON dbo.book_copies.branch_id = dbo.library_branch.branch_id
+WHERE dbo.book.title = 'the lost tribe' 
+
+GO
